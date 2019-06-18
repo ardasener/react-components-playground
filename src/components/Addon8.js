29e8aa1cc006cca3fc93a2def20bd53c52766067
ardasener <ardasener@sabanciuniv.edu>
2019-06-18 14:38:47 +0300

    Addon 8 - Button Group

--- src/components/Addon8.js
+++ src/components/Addon8.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import Button from 'react-bootstrap/Button'
+
+const scope = {ButtonGroup, Button}
 
 class Addon8 extends Component {
     render() {
         return (
             <div>
-                <FiddleComponent code="<div>this is Addon8</div>" ></FiddleComponent>
+                <FiddleComponent code='
+                    <ButtonGroup aria-label="Basic example">
+                    <Button variant="secondary">One</Button>
+                    <Button variant="primary">Two</Button>
+                    <Button variant="secondary">Three</Button>
+                    </ButtonGroup>'
+                scope={scope} ></FiddleComponent>
             </div>
         );
     }
